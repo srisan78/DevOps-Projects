2197cb499731f8ad88f78a7bfd6cfadba067fd15
srisan78 <117240552+srisan78@users.noreply.github.com>
2026-02-19 15:02:09 +0530

    Update variables.tf

--- DevOps-Project-11/modules/aws-rds/variables.tf
+++ DevOps-Project-11/modules/aws-rds/variables.tf
@@ -1,7 +1,7 @@
-variable "sg-name" {}
-variable "private-subnet-name1" {}
-variable "private-subnet-name2" {}
-variable "db-sg-name" {}
+variable "sg-name" {}               # used in db_subnet_group
+variable "private-subnet1" {}       # use this instead of private-subnet-name1
+variable "private-subnet2" {}       # use this instead of private-subnet-name2
+variable "db_sg_id" {}              
 variable "rds-username" {}
 variable "rds-pwd" {}
 variable "db-name" {}
