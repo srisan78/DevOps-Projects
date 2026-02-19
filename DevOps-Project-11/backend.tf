d87fe5e0737d7869a3567775cb147729249133c1
srisan78 <117240552+srisan78@users.noreply.github.com>
2026-02-19 10:14:59 +0530

    Update backend.tf

--- DevOps-Project-11/backend.tf
+++ DevOps-Project-11/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "sri-9500"
     region         = "us-east-1"
     key            = "DevOps Project-11/terraform.tfstate"
     dynamodb_table = "Lock-Files"
