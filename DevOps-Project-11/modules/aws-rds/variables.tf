7cd149493a7974111176df1d496021256c35e302
srisan78 <117240552+srisan78@users.noreply.github.com>
2026-02-19 14:13:52 +0530

    Update variables.tf

--- DevOps-Project-11/modules/aws-rds/variables.tf
+++ DevOps-Project-11/modules/aws-rds/variables.tf
@@ -1,8 +1,15 @@
-variable "sg-name" {}
-variable "private-subnet-name1" {}
-variable "private-subnet-name2" {}
-variable "db-sg-name" {}
-variable "rds-username" {}
-variable "rds-pwd" {}
-variable "db-name" {}
-variable "rds-name" {}
+variable "cluster_identifier" {}
+variable "engine_version" {}
+variable "master_username" {}
+variable "master_password" {}
+variable "database_name" {}
+variable "db_subnet_group_name" {}
+variable "private_subnet1" {}
+variable "private_subnet2" {}
+variable "db_sg_id" {}
+variable "instance_class" {
+  default = "db.r5.large"
+}
+variable "read_replica_count" {
+  default = 1
+}
